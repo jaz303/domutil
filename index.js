bf2ec309688de5bc1e73a840a1a4c931f3b956ac
Jason Frame <jason@onehackoranother.com>
2014-02-16 02:21:24 +0000

    use classList if available, remove data methods

--- index.js
+++ index.js
@@ -1,142 +1,149 @@
-// Constants from jQuery
-var rclass = /[\t\r\n]/g;
-var core_rnotwhite = /\S+/g;
-
-var DataStore         = {},
-    kDataStoreNextIx  = 1,
-    kDataKey          = 'du-data-key';
-
 var __window = typeof window === 'undefined'
-                ? null
-                : window;
+                                ? null
+                                : window;
 
 var __document = typeof document === 'undefined'
-                  ? null
-                  : document;
+                                    ? null
+                                    : document;
+
+if (typeof DOMTokenList !== 'undefined') {
+
+    function hasClass(el, className) {
+        return el.classList.contains(className);
+    }
+
+    function addClass(el, classes) {
+        if (classes.indexOf(' ') >= 0) {
+            classes.split(/\s+/).forEach(function(c) {
+                el.classList.add(c);
+            });
+        } else {
+            el.classList.add(classes);
+        }
+    }
+
+    function removeClass(el, classes) {
+        if (classes.indexOf(' ') >= 0) {
+            classes.split(/\s+/).forEach(function(c) {
+                el.classList.remove(c);
+            });
+        } else {
+            el.classList.remove(classes);
+        }
+    }
+
+    function toggleClass(el, classes) {
+        if (classes.indexOf(' ') >= 0) {
+            classes.split(/\s+/).forEach(function(c) {
+                el.classList.toggle(c);
+            });
+        } else {
+            el.classList.toggle(classes);
+        }
+    }
+
+} else {
+
+    // Constants from jQuery
+    var rclass = /[\t\r\n]/g;
+    var core_rnotwhite = /\S+/g;
+
+    // from jQuery
+    function hasClass(ele, className) {
+        className = " " + className + " ";
+        return (" " + ele.className + " ").replace(rclass, " ").indexOf(className) >= 0;
+    }
+
+    function addClass(ele, value) {
+        var classes = (value || "").match(core_rnotwhite) || [],
+                cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
+
+        if (cur) {
+            var j = 0, clazz;
+            while ((clazz = classes[j++])) {
+                if (cur.indexOf(" " + clazz + " ") < 0) {
+                    cur += clazz + " ";
+                }
+            }
+            ele.className = cur.trim();
+        }
+    }
+
+    function removeClass(ele, value) {
+        var classes = (value || "").match(core_rnotwhite) || [],
+                cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
+
+        if (cur) {
+            var j = 0, clazz;
+            while ((clazz = classes[j++])) {
+                while (cur.indexOf(" " + clazz + " ") >= 0) {
+                    cur = cur.replace(" " + clazz + " ", " ");
+                }
+                ele.className = value ? cur.trim() : "";
+            }
+        }
+    }
+
+    function toggleClass(ele, value) {
+        var classes = (value || "").match(core_rnotwhite) || [],
+                cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
+
+        if (cur) {
+            var j = 0, clazz;
+            while ((clazz = classes[j++])) {
+                var removeCount = 0;
+                while (cur.indexOf(" " + clazz + " ") >= 0) {
+                    cur = cur.replace(" " + clazz + " ", " ");
+                    removeCount++;
+                }
+                if (removeCount === 0) {
+                    cur += clazz + " ";
+                }
+                ele.className = cur.trim();
+            }
+        }
+    }
+
+}
 
 function generateElementKey() {
-  return kDataStoreNextIx++;
+    return kDataStoreNextIx++;
 }
 
 module.exports = {
-  init: function(window, document) {
-    __window = window;
-    __document = document;
-  },
+    init: function(window, document) {
+        __window = window;
+        __document = document;
+    },
 
-  data: function(el, key, val) {
-    var elementKey = el.getAttribute(kDataKey);
-    if (!elementKey) {
-      elementKey = generateElementKey();
-      el.setAttribute(kDataKey, elementKey);
+    hasClass: hasClass,
+    addClass: addClass,
+    removeClass: removeClass,
+    toggleClass: toggleClass,
+
+    viewportSize: function() {
+        return {
+            width: __document.documentElement.clientWidth,
+            height: __document.documentElement.clientHeight
+        };
+    },
+
+    stop: function(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+    },
+
+    setPosition: function(el, x, y) {
+        el.style.left = x + 'px';
+        el.style.top = y + 'px';
+    },
+
+    setSize: function(width, height) {
+        el.style.width = width + 'px';
+        el.style.height = height + 'px';
+    },
+
+    isElement: function(el) {
+        return el && el.nodeType === 1;
     }
-
-    var elementData = DataStore[elementKey];
-    
-    if (arguments.length === 2) {
-      if (typeof key === 'undefined') {
-        delete DataStore[elementKey];
-      } else {
-        return elementData ? elementData[key] : undefined;
-      }
-    } else if (arguments.length === 3) {
-      if (typeof val === 'undefined') {
-        if (elementData) {
-          delete elementData[key];
-        }
-      } else {
-        if (!elementData) {
-          elementData = {};
-          DataStore[elementKey] = elementData;
-        }
-        elementData[key] = val;
-      }
-    } else {
-      throw "data() - invalid arguments";
-    }
-  },
-
-  // from jQuery
-  hasClass: function(ele, className) {
-    className = " " + className + " ";
-    return (" " + ele.className + " ").replace(rclass, " ").indexOf(className) >= 0;
-  },
-
-  // from jQuery
-  addClass: function(ele, value) {
-    var classes = (value || "").match(core_rnotwhite) || [],
-        cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
-
-    if (cur) {
-      var j = 0, clazz;
-      while ((clazz = classes[j++])) {
-        if (cur.indexOf(" " + clazz + " ") < 0) {
-          cur += clazz + " ";
-        }
-      }
-      ele.className = cur.trim();
-    }
-  },
-
-  // from jQuery
-  removeClass: function(ele, value) {
-    var classes = (value || "").match(core_rnotwhite) || [],
-        cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
-
-    if (cur) {
-      var j = 0, clazz;
-      while ((clazz = classes[j++])) {
-        while (cur.indexOf(" " + clazz + " ") >= 0) {
-          cur = cur.replace(" " + clazz + " ", " ");
-        }
-        ele.className = value ? cur.trim() : "";
-      }
-    }
-  },
-
-  toggleClass: function(ele, value) {
-    var classes = (value || "").match(core_rnotwhite) || [],
-        cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
-
-    if (cur) {
-      var j = 0, clazz;
-      while ((clazz = classes[j++])) {
-        var removeCount = 0;
-        while (cur.indexOf(" " + clazz + " ") >= 0) {
-          cur = cur.replace(" " + clazz + " ", " ");
-          removeCount++;
-        }
-        if (removeCount === 0) {
-          cur += clazz + " ";
-        }
-        ele.className = cur.trim();
-      }
-    }
-  },
-
-  viewportSize: function() {
-    return {
-      width: __document.documentElement.clientWidth,
-      height: __document.documentElement.clientHeight
-    };
-  },
-
-  stop: function(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-  },
-
-  setPosition: function(el, x, y) {
-    el.style.left = x + 'px';
-    el.style.top = y + 'px';
-  },
-
-  setSize: function(width, height) {
-    el.style.width = width + 'px';
-    el.style.height = height + 'px';
-  },
-
-  isElement: function(el) {
-    return el && el.nodeType === 1;
-  }
 };
