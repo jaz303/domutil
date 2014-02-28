07815da53a97ef9c587ff3f5d102a5f95a54c4a5
Jason Frame <jason@onehackoranother.com>
2014-02-28 09:10:15 +0000

    remove initialisation/global document/windows, fix up classlist support

--- index.js
+++ index.js
@@ -1,130 +1,21 @@
-var __window = typeof window === 'undefined'
-                                ? null
-                                : window;
-
-var __document = typeof document === 'undefined'
-                                    ? null
-                                    : document;
+var clazz;
 
 if (typeof DOMTokenList !== 'undefined') {
-
-    function hasClass(el, className) {
-        return el.classList.contains(className);
-    }
-
-    function addClass(el, classes) {
-        if (classes.indexOf(' ') >= 0) {
-            classes.split(/\s+/).forEach(function(c) {
-                el.classList.add(c);
-            });
-        } else {
-            el.classList.add(classes);
-        }
-    }
-
-    function removeClass(el, classes) {
-        if (classes.indexOf(' ') >= 0) {
-            classes.split(/\s+/).forEach(function(c) {
-                el.classList.remove(c);
-            });
-        } else {
-            el.classList.remove(classes);
-        }
-    }
-
-    function toggleClass(el, classes) {
-        if (classes.indexOf(' ') >= 0) {
-            classes.split(/\s+/).forEach(function(c) {
-                el.classList.toggle(c);
-            });
-        } else {
-            el.classList.toggle(classes);
-        }
-    }
-
+    clazz = require('./impl/classes-classlist.js');
 } else {
-
-    // Constants from jQuery
-    var rclass = /[\t\r\n]/g;
-    var core_rnotwhite = /\S+/g;
-
-    // from jQuery
-    function hasClass(ele, className) {
-        className = " " + className + " ";
-        return (" " + ele.className + " ").replace(rclass, " ").indexOf(className) >= 0;
-    }
-
-    function addClass(ele, value) {
-        var classes = (value || "").match(core_rnotwhite) || [],
-                cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
-
-        if (cur) {
-            var j = 0, clazz;
-            while ((clazz = classes[j++])) {
-                if (cur.indexOf(" " + clazz + " ") < 0) {
-                    cur += clazz + " ";
-                }
-            }
-            ele.className = cur.trim();
-        }
-    }
-
-    function removeClass(ele, value) {
-        var classes = (value || "").match(core_rnotwhite) || [],
-                cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
-
-        if (cur) {
-            var j = 0, clazz;
-            while ((clazz = classes[j++])) {
-                while (cur.indexOf(" " + clazz + " ") >= 0) {
-                    cur = cur.replace(" " + clazz + " ", " ");
-                }
-                ele.className = value ? cur.trim() : "";
-            }
-        }
-    }
-
-    function toggleClass(ele, value) {
-        var classes = (value || "").match(core_rnotwhite) || [],
-                cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
-
-        if (cur) {
-            var j = 0, clazz;
-            while ((clazz = classes[j++])) {
-                var removeCount = 0;
-                while (cur.indexOf(" " + clazz + " ") >= 0) {
-                    cur = cur.replace(" " + clazz + " ", " ");
-                    removeCount++;
-                }
-                if (removeCount === 0) {
-                    cur += clazz + " ";
-                }
-                ele.className = cur.trim();
-            }
-        }
-    }
-
-}
-
-function generateElementKey() {
-    return kDataStoreNextIx++;
+    clazz = require('./impl/classes-string.js');
 }
 
 module.exports = {
-    init: function(window, document) {
-        __window = window;
-        __document = document;
-    },
+    hasClass: clazz.hasClass,
+    addClass: clazz.addClass,
+    removeClass: clazz.removeClass,
+    toggleClass: clazz.toggleClass,
 
-    hasClass: hasClass,
-    addClass: addClass,
-    removeClass: removeClass,
-    toggleClass: toggleClass,
-
-    viewportSize: function() {
+    viewportSize: function(doc) {
         return {
-            width: __document.documentElement.clientWidth,
-            height: __document.documentElement.clientHeight
+            width: doc.documentElement.clientWidth,
+            height: doc.documentElement.clientHeight
         };
     },
 
