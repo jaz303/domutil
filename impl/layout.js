61a60d97752924f8969f04a765cafd4a71946fe8
Jason Frame <jason@onehackoranother.com>
2014-07-10 13:51:20 +0100

    setRect()

--- impl/layout.js
+++ impl/layout.js
@@ -1,3 +1,10 @@
+exports.setRect = function(el, x, y, width, height) {
+	el.style.left = x + 'px';
+    el.style.top = y + 'px';
+    el.style.width = width + 'px';
+    el.style.height = height + 'px';
+}
+
 exports.setPosition = function(el, x, y) {
     el.style.left = x + 'px';
     el.style.top = y + 'px';
