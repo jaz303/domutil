58b67e6746c8e025ffcdbdb7496f959df2a59f63
Jason Frame <jason@onehackoranother.com>
2014-09-09 12:09:53 +0100

    cull old tests

--- test/bundle.js
+++ test/bundle.js
@@ -1765,152 +1765,6 @@ function click(el) {
     return evt;
 }
 
-test("bind()", function(assert) {
-
-    var el = document.querySelector('#bind a');
-    var x = 0;
-
-    du.bind(el, 'click', function(evt) {
-        x++;
-    });
-
-    click(el);
-
-    assert.ok(x === 1);
-    assert.end();
-
-});
-
-test("unbind()", function(assert) {
-
-    var el = document.querySelector('#unbind a');
-    var x = 0;
-
-    var fn = du.bind(el, 'click', function(evt) {
-        x++;
-    });
-
-    du.unbind(el, 'click', fn);
-
-    click(el);
-
-    assert.ok(x === 0);
-    assert.end();
-
-});
-
-test("bind_c() - bind", function(assert) {
-
-    var el = document.querySelector('#bind_c_1 a');
-    var x = 0;
-
-    du.bind_c(el, 'click', function(evt) {
-        x++;
-    });
-
-    click(el);
-
-    assert.ok(x === 1);
-    assert.end();
-
-});
-
-test("bind_c() - cancellation", function(assert) {
-
-    var el = document.querySelector('#bind_c_2 a');
-    var x = 0;
-
-    var cancel = du.bind_c(el, 'click', function(evt) {
-        x++;
-    });
-
-    cancel();
-
-    click(el);
-
-    assert.ok(x === 0);
-    assert.end();
-
-});
-
-test("delegate() - bind", function(assert) {
-
-    var el = document.querySelector('#delegate_1');
-    
-    var x = 0;
-    du.delegate(el, 'click', 'a', function(evt) {
-        assert.ok(this === el, "this in delegate == original element");
-        x += parseInt(evt.delegateTarget.getAttribute('data-val'));
-    });
-
-    click(document.querySelector('#delegate_1 div > :nth-child(1) span'));
-    click(document.querySelector('#delegate_1 div > :nth-child(2) span'));
-    click(document.querySelector('#delegate_1 div > :nth-child(3) span'));
-
-    assert.ok(x === 5, "x should == 5");
-    assert.end();
-
-});
-
-test("delegate() - unbind", function(assert) {
-
-    var el = document.querySelector('#delegate_2');
-    
-    var x = 0;
-    var fn = du.delegate(el, 'click', 'a', function(evt) {
-        x += parseInt(evt.delegateTarget.getAttribute('data-val'));
-    });
-
-    du.unbind(el, 'click', fn);
-
-    click(document.querySelector('#delegate_2 div > :nth-child(1) span'));
-    click(document.querySelector('#delegate_2 div > :nth-child(2) span'));
-    click(document.querySelector('#delegate_2 div > :nth-child(3) span'));
-
-    assert.ok(x === 0, "x should == 0");
-    assert.end();
-
-});
-
-test("delegate_c() - bind", function(assert) {
-
-    var el = document.querySelector('#delegate_c_1');
-    
-    var x = 0;
-    du.delegate_c(el, 'click', 'a', function(evt) {
-        assert.ok(this === el, "this in delegate == original element");
-        x += parseInt(evt.delegateTarget.getAttribute('data-val'));
-    });
-
-    click(document.querySelector('#delegate_c_1 div > :nth-child(1) span'));
-    click(document.querySelector('#delegate_c_1 div > :nth-child(2) span'));
-    click(document.querySelector('#delegate_c_1 div > :nth-child(3) span'));
-
-    assert.ok(x === 5, "x should == 5");
-    assert.end();
-
-});
-
-test("delegate_c() - unbind", function(assert) {
-
-    var el = document.querySelector('#delegate_c_2');
-    
-    var x = 0;
-    var fn = du.delegate_c(el, 'click', 'a', function(evt) {
-        x += parseInt(evt.delegateTarget.getAttribute('data-val'));
-    });
-
-    fn();
-
-    click(document.querySelector('#delegate_c_2 div > :nth-child(1) span'));
-    click(document.querySelector('#delegate_c_2 div > :nth-child(2) span'));
-    click(document.querySelector('#delegate_c_2 div > :nth-child(3) span'));
-
-    assert.ok(x === 0, "x should == 0");
-    assert.end();
-
-});
-
 test("stop()", function(assert) {
 
     var el = document.querySelector('#stop');
@@ -1930,25 +1784,11 @@ test("stop()", function(assert) {
 },{"../":"/Users/jason/dev/projects/domutil/index.js","tape":"/Users/jason/dev/projects/domutil/node_modules/tape/index.js"}],"/Users/jason/dev/projects/domutil/test/main.js":[function(require,module,exports){
 require('./classes');
 require('./events');
-require('./matches_selector');
 require('./node');
 require('./style');
 require('./text');
 
-},{"./classes":"/Users/jason/dev/projects/domutil/test/classes.js","./events":"/Users/jason/dev/projects/domutil/test/events.js","./matches_selector":"/Users/jason/dev/projects/domutil/test/matches_selector.js","./node":"/Users/jason/dev/projects/domutil/test/node.js","./style":"/Users/jason/dev/projects/domutil/test/style.js","./text":"/Users/jason/dev/projects/domutil/test/text.js"}],"/Users/jason/dev/projects/domutil/test/matches_selector.js":[function(require,module,exports){
-var du = require('../');
-var test = require('tape');
-
-test("matchesSelector()", function(assert) {
-
-    var el = document.getElementById('title');
-
-    assert.ok(du.matchesSelector('div h1 span', el));
-    assert.notOk(du.matchesSelector('table', el));
-    assert.end();
-
-});
-},{"../":"/Users/jason/dev/projects/domutil/index.js","tape":"/Users/jason/dev/projects/domutil/node_modules/tape/index.js"}],"/Users/jason/dev/projects/domutil/test/node.js":[function(require,module,exports){
+},{"./classes":"/Users/jason/dev/projects/domutil/test/classes.js","./events":"/Users/jason/dev/projects/domutil/test/events.js","./node":"/Users/jason/dev/projects/domutil/test/node.js","./style":"/Users/jason/dev/projects/domutil/test/style.js","./text":"/Users/jason/dev/projects/domutil/test/text.js"}],"/Users/jason/dev/projects/domutil/test/node.js":[function(require,module,exports){
 var du = require('../');
 var test = require('tape');
 
